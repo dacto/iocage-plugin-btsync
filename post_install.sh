4bd4d052d32954e7de6b22b4586710ba7641c530
dacto <dacto@users.noreply.github.com>
2023-04-09 10:54:40 -0700

    Use the ports tree

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+set -eu
+
+# Fetch the necessary ports
+portsnap fetch
+portsnap extract Mk
+portsnap extract ports-mgmt/pkg
+portsnap extract net-p2p/rslsync
+
+# Build and install rslsync
+cd /usr/ports/net-p2p/rslsync/
+make install clean
 
 # Enable the service
 sysrc -f /etc/rc.conf rslsync_enable="YES"
