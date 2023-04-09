404cc73e4de8c37c902a650b5b83fb626349dc40
dacto <dacto@users.noreply.github.com>
2023-04-09 12:56:50 -0700

    wip

--- post_install.sh
+++ post_install.sh
@@ -1,15 +1,45 @@
 #!/bin/sh
 set -eu
 
-# Fetch the necessary ports
-portsnap fetch
-portsnap extract Mk
-portsnap extract ports-mgmt/pkg
-portsnap extract net-p2p/rslsync
+VERSION='2.7.3'
 
-# Build and install rslsync
-cd /usr/ports/net-p2p/rslsync/
-make install clean
+export PREFIX=/usr/local
+
+export RSLSYNC_USER=rslsync
+export RSLSYNC_GROUP=rslsync
+export CONF="${PREFIX}/etc/rslsync.conf"
+export COMMAND="${PREFIX}/bin/rslsync"
+
+# Create user and group
+
+
+# Setup service file
+envsubst '$COMMAND:$CONF:$RSL_USER:$RSL_GROUP' \
+    <"${PREFIX}/etc/rslsync/rc.d.template" \
+    >"${PREFIX}/etc/rc.d/rslsync"
+
+chmod +x "${PREFIX}/etc/rc.d/rslsync"
+
+# Download
+SRC='resilio-sync_freebsd_x64.tar.gz'
+SRCDST="/tmp/${SRC}"
+fetch -o "$SRCDEST" \
+    "https://download-cdn.resilio.com/${VERSION}/FreeBSD-x64/${SRC}"
+
+# Install
+tar xjf "$SRCDEST" -C "$(dirname "${COMMAND}")"
+chmod +x "$COMMAND"
+
+# Generate config
+CONF="${PREFIX}/etc/rslsync/rslsync.conf.sample"
+"$COMMAND" --nodaemon --storage /tmp --dump-sample-config >"$CONF"
+sed -i -e 's;^//\([[:space:]]*"storage_path"[[:space:]]*:[[:space:]]*\)"/.*",$$;\1"/var/db/rslsync",;' \
+       -e 's;^//\([[:space:]]*"pid_file"[[:space:]]*:[[:space:]]*\)"/.*",$$;\1"/var/run/rslsync/rslsync.pid",;' \
+    "$CONF"
+
+chown "$RSL_USER":"$RSL_GROUP" "$CONF"
+
+mkdir -p /var/db/rslsync
 
 # Enable the service
 sysrc -f /etc/rc.conf rslsync_enable="YES"
